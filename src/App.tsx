798f1308b9217aa07e6bd2639ebed86d5807d5db
Vivek Maurya <Vivek563maurya@gmail.com>
2024-06-09 18:03:08 +0530

    fix: router issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Dashboard from "./components/dashboard/dashboard";
+import Login from "./components/login/login";
+import Register from "./components/register/register";
+
 function App() {
      return (
-          <>
-               <h1>vivek</h1>
-          </>
-          // <Router>
-          //      <Routes>
-          //           <Route path="/login" element={<Login />} />
-          //           <Route path="/register" element={<Register />} />
-          //           {/* Add other routes here as needed */}
-          //      </Routes>
-          // </Router>
+          <Router>
+               <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    {/* Add other routes here as needed */}
+               </Routes>
+          </Router>
      );
 }
 
