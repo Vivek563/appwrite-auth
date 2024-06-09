9818a8ec8147b49db64de771d3af4144e34167af
Vivek Maurya <Vivek563maurya@gmail.com>
2024-06-09 21:39:16 +0530

    fix: router issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import Dashboard from "./components/dashboard/dashboard";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
@@ -7,18 +7,16 @@ import ProtectedRoute from "./protectrouter";
 function App() {
      return (
           <Router>
-               <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route
-                         path="/"
-                         element={
-                              <ProtectedRoute>
-                                   <Dashboard />
-                              </ProtectedRoute>
-                         }
-                    />
-               </Routes>
+               <Route
+                    path="/"
+                    element={
+                         <ProtectedRoute>
+                              <Dashboard />
+                              <Route path="/login" element={<Login />} />
+                              <Route path="/register" element={<Register />} />
+                         </ProtectedRoute>
+                    }
+               />
           </Router>
      );
 }
