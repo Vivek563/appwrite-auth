4dc213ad30f38fa895c33adca449b07544c82f2f
Vivek Maurya <Vivek563maurya@gmail.com>
2024-06-09 23:12:30 +0530

    fix: router

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import Dashboard from "./components/dashboard/dashboard";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
 import ProtectedRoute from "./protectrouter";
+import RedirectAuthenticated from "./redirectauth";
 
 function App() {
      return (
           <Router>
                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
+                    <Route
+                         path="/login"
+                         element={
+                              <RedirectAuthenticated>
+                                   <Login />
+                              </RedirectAuthenticated>
+                         }
+                    />
+                    <Route
+                         path="/register"
+                         element={
+                              <RedirectAuthenticated>
+                                   <Register />
+                              </RedirectAuthenticated>
+                         }
+                    />
                     <Route
                          path="/"
                          element={
